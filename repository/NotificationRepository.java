af075123430098c6d4f94cc2172f7a1dc8d3c05f
tranvanhai591989 <102501534+tranvanhai591989@users.noreply.github.com>
2022-09-08 17:18:34 +0700

    HaiTV up notification

---
+++ repository/NotificationRepository.java
@@ -0,0 +1,49 @@
+package vn.codegym.pig_farm.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import vn.codegym.pig_farm.dto.INotificationDto;
+import vn.codegym.pig_farm.entity.Notification;
+
+import javax.transaction.Transactional;
+
+@Transactional
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+
+    /**
+     * Create by HaiTV
+     * Date : 08/09/2022
+     * Display : Notification
+     * @param content
+     * @param pageable
+     * @return
+     */
+    @Query(value = " select id , content, submitted_date as submittedDate , image  " +
+            " from notification " +
+            " where content " +
+            "like :content " +
+            "and is_deleted =0",
+            nativeQuery = true,
+            countQuery = "select count(*) from (select id , content, submitted_date as submittedDate , image  " +
+                    " from notification " +
+                    " where content " +
+                    "like :content " +
+                    "and is_deleted =0")
+    Page<INotificationDto> findAllNotification(@Param("content") String content, Pageable pageable);
+
+    /**
+     * Create by HaiTV
+     * Date : 08/09/2022
+     * Delete : Notification
+     * @param id
+     */
+
+    @Modifying
+    @Query(value = "update notification set is_deleted =1 where id =:id", nativeQuery = true)
+    void delete(@Param("id") Integer id);
+
+}
