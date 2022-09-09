e1a50675c56f525466575615ee39eff919b1c56b
Dimoxa <trankimtuongtk@gmail.com>
2022-09-09 16:50:13 +0700

    TuongTK update test uni5

---
+++ pig_farm/src/main/java/vn/codegym/pig_farm/dto/PigDto.java
@@ -0,0 +1,30 @@
+package vn.codegym.pig_farm.dto;
+
+/**
+ * create class PigDto
+ * @author TuongTK
+ * @Date 09/09/2022
+ */
+public class PigDto {
+    private Integer id;
+    private String code;
+
+    public PigDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
