9e18ae3de633326192a8dc31a387a2413f1a0ba0
Dimoxa <trankimtuongtk@gmail.com>
2022-09-09 10:24:20 +0700

    TuongPK updated treatment

---
+++ pig_farm/src/main/java/vn/codegym/pig_farm/service/ITreatmentService.java
@@ -0,0 +1,13 @@
+package vn.codegym.pig_farm.service;
+
+import vn.codegym.pig_farm.entity.Treatment;
+
+public interface ITreatmentService {
+    /**
+     * create by TuongTK
+     * date: 08/09/2022
+     * create method save(Treatment treatment)
+     * @param treatment
+     */
+    void save(Treatment treatment);
+}
