7427da24fc2609dbcf9eb72e7916e91fe09d4176
tranvanhai591989 <102501534+tranvanhai591989@users.noreply.github.com>
2022-09-09 10:22:01 +0700

    HaiTV up notification-management

---
+++ pig_farm/src/main/java/vn/codegym/pig_farm/entity/Notification.java
@@ -0,0 +1,32 @@
+package vn.codegym.pig_farm.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Notification {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "VARCHAR(1000)")
+    private String content;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate submittedDate;
+
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String image;
+
+    @Column(columnDefinition = "BIT")
+    private Boolean isDeleted;
+}
